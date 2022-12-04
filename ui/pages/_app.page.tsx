cd74a78f9dcaac00ce4645ccfa3f4c797ac45888
Shawn Pereira <shawnpereira1999@gmail.com>
2022-12-04 05:54:21 +0530

    initial commit

--- ui/pages/_app.page.tsx
+++ ui/pages/_app.page.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
